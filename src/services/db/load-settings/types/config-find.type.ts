fefdcd1688460b3f8b73d431706640b7d566b7a8
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2026-06-07 08:43:57 -0300

    feat: add cached configuration service to retrieve and validate database settings by ID

---
+++ src/services/db/load-settings/types/config-find.type.ts
@@ -0,0 +1,24 @@
+import type { RowDataPacket } from "mysql2/promise";
+
+export type ConfigFindByIdItem = RowDataPacket & {
+  CONFIG_ID: number;
+  PROJECT_ID: number | null;
+  CUSTOMER_NAME: string | null;
+  TELEGRAM_BOT_NAME: string | null;
+  TELEGRAM_BOT_LINK: string | null;
+  TELEGRAM_BOT_TOKEN: string | null;
+  TELEGRAM_BOT_CHATID: string | null;
+  WEBHOOK_URL: string | null;
+  WEBHOOK_LOCAL_PORT: number | null;
+  OPENAI_API_KEY: string | null;
+  SHOPEE_CREDENTIAL: string | null;
+  SHOPEE_SECRET_KEY: string | null;
+  SHOPEE_AFFILIATE_ENDPOINT: string | null;
+  SHOPEE_AFFILIATE_TIMEOUT: string | null;
+  SHOPEE_AFFILIATE_SUBIDS: string | null;
+  SHOPEE_PAGE: number | null;
+  SHOPEE_SORTTYPE: number | null;
+  SHOPEE_LIMIT: number | null;
+  CREATEDAT: Date | null;
+  UPDATEDAT: Date | null;
+};
